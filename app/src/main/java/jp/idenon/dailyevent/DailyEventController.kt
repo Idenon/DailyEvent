c157a38916aa1ea3515bff18daafd0c659594798
Hiroshi Ideno <ideno@cloudpack.jp>
2019-05-28 12:46:43 +0900

    実行テスト用のボタンを作成

---
+++ app/src/main/java/jp/idenon/dailyevent/DailyEventController.kt
@@ -0,0 +1,9 @@
+package jp.idenon.dailyevent
+
+class DailyEventController {
+
+    // デイリー範囲内で実行済みかどうかを返す
+    fun isDoneDaily(): Boolean {
+        return false
+    }
+}
--- app/src/main/java/jp/idenon/dailyevent/MainActivity.kt
+++ app/src/main/java/jp/idenon/dailyevent/MainActivity.kt
@@ -2,11 +2,25 @@ package jp.idenon.dailyevent
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val event = DailyEventController()
+
+        button.setOnClickListener{
+            if (event.isDoneDaily()) {
+                // true だった時の処理
+                Toast.makeText(applicationContext, "Done.", Toast.LENGTH_LONG).show()
+            } else {
+                // false だった時の処理
+                Toast.makeText(applicationContext, "Not Done.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
